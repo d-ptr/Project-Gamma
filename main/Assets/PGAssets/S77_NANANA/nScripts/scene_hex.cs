94474d50fe97358d1023f8f62c561d995101bc15
Wyman Wong <Kegaskn@gmail.com>
2015-11-15 03:02:44 +0800

    add Hex Coordinate

--- main/Assets/PGAssets/S77_NANANA/nScripts/scene_hex.cs
+++ main/Assets/PGAssets/S77_NANANA/nScripts/scene_hex.cs
@@ -1,15 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class scene_hex : MonoBehaviour {
+public class scene_hex : MonoBehaviour 
+{
+    
+    public GameObject groundParent;
+    public GameObject ground;
 
-	// Use this for initialization
-	void Start () {
-	
+
+	void Start () 
+    {
+        _createGround(HexCoord.ORIGINAL.CartesianCoord * 0.38f);
+        _createGround(HexCoord.TOP_LEFT.CartesianCoord * 0.38f);
+        _createGround(HexCoord.TOP_MIDDLE.CartesianCoord * 0.38f);
+        _createGround(HexCoord.TOP_RIGHT.CartesianCoord * 0.38f);
+        _createGround(HexCoord.BOTTOM_LEFT.CartesianCoord * 0.38f);
+        _createGround(HexCoord.BOTTOM_MIDDLE.CartesianCoord * 0.38f);
+        _createGround(HexCoord.BOTTOM_RIGHT.CartesianCoord * 0.38f);
 	}
+    
+    private void _createGround(Vector2 pos)
+    {
+        GameObject _ground = Instantiate(ground);
+
+        _ground.transform.parent = groundParent.transform;
+        _ground.transform.localPosition = new Vector3(pos.x, 0, pos.y);
+    }
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
 }
